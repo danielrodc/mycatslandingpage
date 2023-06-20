0c0b72c9ef74460c5f34541b0367dd1c092bd910
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-06-20 00:57:52 +0000

    my changes

---
+++ src/js/component/Bottom.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Bottom = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container-display-flex justify-content">
+          <a className="text-white text-size-small" href="#">
+            Copyrights owned by Cats LLC 
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Bottom;
